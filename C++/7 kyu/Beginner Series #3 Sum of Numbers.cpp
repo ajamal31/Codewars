1abe73ab05152029db5277ef6d63e8b7d3183d2e
Arun Jamal <ajamal31@hotmail.com>
2023-02-04 18:18:30 -0700

    Add C++ Solutions

---
+++ C++/7 kyu/Beginner Series #3 Sum of Numbers.cpp
@@ -0,0 +1,25 @@
+// Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+// Note: a and b are not ordered!
+
+// Examples (a, b) --> output (explanation)
+// (1, 0) --> 1 (1 + 0 = 1)
+// (1, 2) --> 3 (1 + 2 = 3)
+// (0, 1) --> 1 (0 + 1 = 1)
+// (1, 1) --> 1 (1 since both are same)
+// (-1, 0) --> -1 (-1 + 0 = -1)
+// (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+
+#include <algorithm>
+
+int get_sum(int a, int b)
+{
+    signed int sum = 0;
+
+    for (signed int i = std::min(a, b); i <= std::max(a, b); i++)
+    {
+        sum += i;
+    }
+
+    return sum;
+}
